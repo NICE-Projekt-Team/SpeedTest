b330880fe5f6c6b819fe4d84cdd008d3d7346572
Luca Schöny <earthwalker31@gmx.at>
2020-07-14 22:03:00 +0200

    Updated GoSpeedTest.go *added comments *now using goroutines

--- GoSpeedTest/GoSpeedTest.go
+++ GoSpeedTest/GoSpeedTest.go
@@ -1,32 +1,102 @@
-package GoSpeedTest
+package main
 
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
-func main() {
-
-	var byteArray [800000000]byte //100 Megabyte
-	byteArray2 := byteArray
+func writeArray(size int, wg *sync.WaitGroup, result chan<- []byte) {
+	fmt.Println("Channel opened, Starting to write Array")
+	var byteArray = make([]byte, size) //creating the array
 	s1 := rand.NewSource(time.Now().UnixNano())
-	r1 := rand.New(s1)
-	var startTime time.Time
+	r1 := rand.New(s1)          //defining the random number generator
+	for i := 0; i < size; i++ { //looping through the array
+		byteArray[i] = byte(r1.Intn(2)) //setting a random value
+	}
+	fmt.Println("Done writing array")
+	result <- byteArray //sending data back through channel
+	defer wg.Done()     //telling the waitgroup that the routine is finished
+}
+
+func transfer(size int, wg *sync.WaitGroup, input []byte, result chan<- []byte) {
+	fmt.Println("Channel opened, starting to transfer array")
+	var byteArray = make([]byte, size)
+	for i := 0; i < size; i++ { //looping through the array
+		byteArray[i] = input[i]
+	}
+	result <- byteArray
+	defer wg.Done()
+
+}
+
+func main() {
+	var size = 80000000 //Setting the size of the Arrays
+
+	var wg sync.WaitGroup //creating the waitgroup
+
+	var byteArray1 = make([]byte, size)
+	var byteArray2 = make([]byte, size) //Creating the 3 byteArrays
+	var byteArray3 = make([]byte, size)
+
+	var startTime time.Time //Creating the start and end Time variable
 	var endTime time.Time
 
-	fmt.Println("Starting to write Array")
-	startTime = time.Now()
-	for i := 0; i < len(byteArray); i++ {
-		byteArray[i] = byte(r1.Intn(2))
-	}
-	endTime = time.Now()
-	fmt.Println("Done writing Array. Duration: ", endTime.Sub(startTime), ", Speed: ", len(byteArray)/int(endTime.Sub(startTime).Milliseconds())/1000, "mBit")
+	ch1 := make(chan []byte, 1)
+	ch2 := make(chan []byte, 1) //Creating the 3 byteArray1-Channels
+	ch3 := make(chan []byte, 1)
 
-	fmt.Println("Starting to transfer Array")
-	startTime = time.Now()
-	byteArray2 = byteArray //data is passed by value not reference
-	endTime = time.Now()
-	fmt.Println("Done transferring Array. Duration: ", endTime.Sub(startTime), ", Speed: ", len(byteArray)/int(endTime.Sub(startTime).Milliseconds())/1000, "mBit      ", byteArray2[1]) // Go forces me to use the byteArray2
+	fmt.Println("Starting to write Arrays")
+	startTime = time.Now() //Starting the timer
 
+	wg.Add(1)                     //add 1 task to the GoRoutine
+	go writeArray(size, &wg, ch1) //define goroutine
+	wg.Add(1)
+	go writeArray(size, &wg, ch2)
+	wg.Add(1)
+	go writeArray(size, &wg, ch3)
+
+	byteArray1 = <-ch1
+	byteArray2 = <-ch2 //receiving data from channels
+	byteArray3 = <-ch3
+
+	wg.Wait() //waiting until all routines are finished
+
+	close(ch1)
+	close(ch2) //closing channel
+	close(ch3)
+
+	endTime = time.Now() //setting end time
+	var duration = endTime.Sub(startTime)
+	fmt.Println("Done writing Arrays with routines. Duration: ", duration)
+
+	//now transferring
+
+	fmt.Println("Starting to transfer Arrays")
+	startTime = time.Now()
+
+	ch4 := make(chan []byte, 1)
+	ch5 := make(chan []byte, 1) //Creating the 3 byteArray1-Channels
+	ch6 := make(chan []byte, 1)
+
+	wg.Add(1)                               //add 1 task to the GoRoutine
+	go transfer(size, &wg, byteArray1, ch4) //define goroutine
+	wg.Add(1)
+	go transfer(size, &wg, byteArray2, ch5)
+	wg.Add(1)
+	go transfer(size, &wg, byteArray3, ch6)
+
+	byteArray1 = <-ch1
+	byteArray2 = <-ch2 //receiving data from channels
+	byteArray3 = <-ch3
+
+	wg.Wait() //waiting until all routines are done
+
+	close(ch4)
+	close(ch5) //closing channels
+	close(ch6)
+
+	endTime = time.Now()
+	fmt.Println("Done transferring Array. Duration: ", endTime.Sub(startTime))
 }
